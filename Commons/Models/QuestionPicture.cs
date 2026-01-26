fbd8d753cf708207a0725581c497aa2023cd8422
Tobias <115145472+TobiasDK22@users.noreply.github.com>
2026-01-26 13:34:16 +0100

    QuestionPicture class added

---
+++ Commons/Models/QuestionPicture.cs
@@ -0,0 +1,9 @@
+﻿namespace Commons.Models
+{
+    public class QuestionPicture
+    {
+        public int Id { get; set; }
+        public int QuestionnaireId { get; set; }
+        public string QPictureBase64 { get; set; }
+    }
+}
