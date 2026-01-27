bda76aad89a7f0c183cfea5d7b087f086d3ade1c
Tobias <115145472+TobiasDK22@users.noreply.github.com>
2026-01-27 14:12:52 +0100

    Created AuthController with simple user registering and added timestamp handling to DbContext.

--- Commons/Models/Common.cs
+++ Commons/Models/Common.cs
@@ -5,11 +5,19 @@ using System.Text;
 
 namespace Commons.Models
 {
-	public class Common<T> 
+	public abstract class Common<T> 
 	{
 		[Key]
 		public T Id { get; set; }
 		public DateTime CreatedAt { get; set; }
 		public DateTime UpdatedAt { get; set; }
-	}
+
+        protected Common()
+        {
+            if (typeof(T) == typeof(string))
+            {
+                Id = (T)(object)Guid.NewGuid().ToString("N");
+            }
+        }
+    }
 }
