f6145fee984ef725b06d3665aa4e2bee3ac8ac74
StineDk <144903306+StineDk@users.noreply.github.com>
2026-01-27 14:24:52 +0100

    New database migration

--- Commons/Models/User.cs
+++ Commons/Models/User.cs
@@ -7,7 +7,7 @@ namespace Commons.Models
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public DateOnly Birthday { get; set; }
-		public string Description { get; set; }
+		public string? Description { get; set; }
 		public string Email { get; set; }
 		public string HashedPassword { get; set; }
 		public List<TagsEnum> ValueTags { get; set; } = new();
