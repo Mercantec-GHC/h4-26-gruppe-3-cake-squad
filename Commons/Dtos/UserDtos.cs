0e1d8ef3db8eea361a15f6dbe932a53b0e8a0da3
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2026-01-30 10:50:02 +0100

    Made Me and User/id, and it works

---
+++ Commons/Dtos/UserDtos.cs
@@ -0,0 +1,20 @@
+﻿namespace Commons.Dtos
+{
+    public class MeResponseDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly Birthday { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class UserResponseDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+    }
+}
