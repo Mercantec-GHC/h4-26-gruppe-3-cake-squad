f1e2c111ec15a97785282bfbe1d9c242e2f81670
StineDk <144903306+StineDk@users.noreply.github.com>
2026-02-05 09:44:41 +0100

    Add MIgration moving the user visibility to a  database entity. Fixes on the controllers to use the new database entity.

---
+++ Commons/Models/Dtos/UserVisibilityDto.cs
@@ -0,0 +1,10 @@
+﻿using Commons.Enums;
+
+namespace Commons.Models.Dtos
+{
+	public class UserVisibilityRequestDto
+	{
+		public string TargetUserId { get; set; }
+		public UserVisibilityEnum VisibilityEnum { get; set; }
+	}
+}
---
+++ Wavelength/Migrations/20260205084115_MovedUserVisibility.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wavelength.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovedUserVisibility : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsUserVisible",
+                table: "QuestionScores");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Visibility",
+                table: "UserVisibilities",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Visibility",
+                table: "UserVisibilities",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsUserVisible",
+                table: "QuestionScores",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
